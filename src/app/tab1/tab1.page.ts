e6b21add64fbd9c5d4397e32aea78fc3ba50f98b
Giovanna <giovannamsantanaa@gmail.com>
2024-05-29 18:47:24 +0100

    feat: Adicionando img do pokemon e estilizando

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,19 +1,33 @@
+import { NavController } from '@ionic/angular';
 import { PokemonService } from './../service/pokemon.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page  implements OnInit {
   pokemonList: any[] = [];
 
-  constructor(private pokemonService: PokemonService) {}
+  constructor(private pokemonService: PokemonService, private navCtrl: NavController) {}
 
   ngOnInit(){
     this.pokemonService.getPokemonList().subscribe(response => {
       this.pokemonList = response.results;
     });
   }
+
+  openDetailsPokemon(pokemon: any) {
+    this.navCtrl.navigateForward(`/pokemon-detail/${this.getPokemonId(pokemon.url)}`);
+  }
+
+  getPokemonId(url: string | undefined): string {
+    if (!url) {
+      return '';
+    }
+    const id = url.split('/').filter(segment => segment).pop();
+    return id ? id : '';
+  }
+
 }
